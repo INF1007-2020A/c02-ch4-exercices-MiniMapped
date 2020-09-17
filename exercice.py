55e41e500ab5791df5b286a1c1f10b6a950e2197
MiniMapped <marc-alexandre.parent@polymtl.ca>
2020-09-17 17:56:18 -0400

    exos complétés

--- exercice.py
+++ exercice.py
@@ -4,19 +4,27 @@
 import random
 
 def is_even_len(string):
-	return 0xDEADBEEF
+	return len(string) % 2 == 0
 
 
 def get_num_char(string, char):
-	return 0xDEADBABE
+	nombreChar = 0
+	for c in string:
+		nombreChar += 1 if c == char else 0
+	return nombreChar
 
 
 def get_first_part_of_name(name):
-	return ""
+	first_part = name.split("-")[0]
+	capitalized = first_part[0].upper() + first_part[1:].lower()
+	return "Bonjour, " + capitalized
 
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+	animal_choisi = animals[random.randrange(len(animals))]
+	adj_choisi = adjectives[random.randrange(len(adjectives))]
+	fruit_choisi = fruits[random.randrange(len(fruits))]
+	return "Aujourd’hui, j’ai vu un %s s’emparer d’un panier %s plein de %s." % (animal_choisi, adj_choisi, fruit_choisi)
 
 
 if __name__ == "__main__":
@@ -30,7 +38,7 @@ if __name__ == "__main__":
 	parrot = "jean-marc"
 	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
 
-	animals = ("chevreuil", "chien", "pigeon")
-	adjectives = ("rouge", "officiel", "lourd")
-	fruits = ("pommes", "kiwis", "bananes")
+	animals = ("chevreuil", "chien", "pigeon", "dragon", "homme")
+	adjectives = ("rouge", "officiel", "lourd", "bleu", "multicolore", "brisé")
+	fruits = ("pommes", "kiwis", "bananes", "bleuets")
 	print(get_random_sentence(animals, adjectives, fruits))
